8ccba9b8f8d820a694cb561ce008c2379b267bb0
nchohan <nchohan@ubuntu.ubuntu-domain>
2010-11-06 02:49:59 -0700

    updated file

---
+++ static/js/euca.js
@@ -0,0 +1,52 @@
+function notify(message, title){
+  $("#notification").remove();
+  msg = $("<div>").append(message).attr("id","notification");
+  $("#notification-dialog").append(msg);
+  if (title){
+    $("#ui-dialog-title-notification-dialog").html(title);
+  }
+  else{
+    $("#ui-dialog-title-notification-dialog").html("Notification Message");
+  }
+  $("#notification-dialog").dialog("open");
+}
+
+function run_instance_response(data){
+  if (data.success != "true"){
+    notify("There was an error with your request: " + data.error, "Error");
+  }
+  else{
+    notify("Success! Instance id of: " + data.instance_id, "Notification");
+  }
+}
+function post_run_instance(image_id){
+  var type = $("#" + image_id + "-type").val();
+  var key = $("#" + image_id + "-key").val();
+  notify("Request sent <br/>Image:" + image_id + " <br/>Type: " + type + " <br/>Key: " + key + "<br/>Please wait...","Notification");
+  $.post("/runpost", {'image_to_run':image_id, 'key_to_use':key, 
+                              'instance_type':type}, run_instance_response,
+                              "json"); 
+}
+
+function setup_notification_dialog(){
+    var confirmed = function(){
+      $("#notification-dialog").dialog("close");
+    };
+
+    var dialogOpts = {
+      autoOpen: false,
+      draggable: true,
+      hide: 'fold',
+      position: 'center',
+      modal: true,
+      buttons: {
+        "Close": confirmed
+      }
+    };
+
+    $("#notification-dialog").dialog(dialogOpts);
+}
+
+function init_euca(){
+  setup_notification_dialog();
+}
